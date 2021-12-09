2acde5672e2108c4f257905247737a3298d8bd59
Marcoseven <marco.caliciotti3@gmail.com>
2021-12-09 13:36:41 +0100

    Creato file index.php (milestone-1)
    Inserita traccia esercizio nel php sotto forma di commento ed aggiunto codice html, con un header e un main con un div container ciascuno. Importato Bootstrap.

---
+++ milestone-1/index.php
@@ -0,0 +1,43 @@
+<?php
+/* 
+Traccia esercizio:
+Stampare a schermo una decina di dischi musicali (vedi screenshot). Utilizzare: Html, JS, VueJS, PHP
+
+- Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+
+- Seconda Milestone:
+Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+
+- Bonus:
+Attraverso un’altra chiamata api, filtrare gli album per genere
+Info: Per la struttura dati guardate l'endpoint usato nell'esercizio vue-dischi.- 
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Vue Dischi</title>
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <div class="container">
+        </div>
+    </header>
+    <!-- /header -->
+
+    <!-- main -->
+    <main>
+        <div class="container">
+        </div>
+    </main>
+    <!-- /main -->
+</body>
+</html>
